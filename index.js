64b840848f63f705e8795d0b14c2f494780b0ae6
johnnyscript <mostmodernist@gmail.com>
2015-08-26 22:10:10 -0700

    back to the old machine

--- index.js
+++ index.js
@@ -7,10 +7,27 @@ styles.fontSize = '24px'
 styles.letterSpacing = '3px'
 styles.lineHeight = '24px'
 
+//for(x in ui) document.body.removeChild(ui[x])
+
+var max = parseInt(getCSS(ui.leftFront, 'width'))
+
+console.log(max)
+
 module.exports = function(text, styles){
+  graph(text.split(/[\n\r]/))
   var dict = alpha(styles)
   var alphaDict = alpha(text, styles)
   console.log(dict)
   console.log(alphaDict)
 }
 
+function graph(text){
+  var c = true
+  var i = 0
+  var str = '<p>'
+}
+
+
+function getCSS(el, prop){
+  return document.defaultView.getComputedStyle(el).getPropertyValue(prop)
+}
--- public/bundle.js
+++ public/bundle.js
@@ -4,7 +4,7 @@
 var aleph = require('./')
 var ui = require('getids')()
 
-var text = "Ereshkiga\n\nTo begin, I never asked to be the ruler of this realm. I was satisfied with my status as a\n\nsky deity from the moment my father's tears conceived me in the womb of mother ocean.\n\nInnocence lives in the sky, and innocent I was. In an eternal whim I could dissolve into clarity,\n\ncome back as a cloud, watch and listen to the above and below. What a time that was.\n\nThey say all good things must come to an end, and so it did, just look what the Jews did to\n\nus with their “monotheism.” Our powers drained away into one megalomaniac of a deity that no\n\none in the heavenly realm payed much attention to, until, with bitter luck he stumbled upon some\n\ninfluential couple in a tent, whispered in their ears, and sparked our demise, disintegration, and\n\nBut demise, disintegration, and destruction are known to be among my most esteemed\n\ntraits. Not as a sky deity of course, that has more to do with those human ideals that the vast\n\nmajority of you put onto a pampered pedestal, while the honest minority are relegated to worship\n\nmy current status at heavy metal shows and psy trance parties deep in the night.\n\nThe legend goes that it was the dragon Kun that did it to me, the first betrayal by one who\n\nwas supposed to love me. He was my brother, though it is hard to track down proper familial\n\nrelations when the mere mingling of tears and water can bring about birth. I was in a moment of\n\nreconfiguration, transitioning from pure blue dissipation to a more watery form, when a net fine\n\nenough to catch the air itself pulled me. I felt myself sweeping downward, it was the fall of the\n\nvery innocence and kindness that had once formed me into a shady cumulous for a desert traveler.\n\nWith each meter of decent I felt myself solidifying into a ruling form, and a ruler I was to be. By\n\nthe time Kun deserted me on the tombstone throne, I was fully anthropomorphic, and still am\n\nBeing the ruler of the underworld had its joys. Law giving came easily to me, as well as\n\ngiving judgements, holding down security matters, and so on and so forth. Perhaps that is why no\n\none has come to my rescue, they recognize my utmost capacity to preside over the dead. I do like\n\nto watch them dance though, swaying forms, slowing decomposing, a twist to the left as the arms\n\nflings off to the right. The dead love to dance here, it speeds up the process of decomposition, and\n\nno one wants to stay here too long. There are more ethereal realms to be explored, where body is\n\nbut a hazy memory cloaked in poetry.\n\nHow many I have seen, kings and emperors, those worshiped as idols, all come here, until\n\nthey are returned to pure earth, no doubt feeding the all loving powers of my bitch of a sister\n\nIshtar, “oh great bringer of life and abundance!”\n\nI should be the goddess of life and abundance, without my kingdom how would your\n\ngardens grow? I hear Shiva had a similar existential crisis once, that God of destruction, he\n\nbecome distraught confiding in his dear Parvati, “Oh love, what am I? Only destruction? where is\n\nmy goodness?” “Lord” she is said to have answered, “Without your destruction where would\n\ncreation be, you are destruction, but from you creation is birthed!” What a wise wise goddess, no\n\none in my pantheon gives me such credit. I admire her beautiful combination of love and wisdom,\n\nthough I never got the chance to meet her in person (I am not allowed to leave my subterranean\n\nrealm). But my great servant Neti came back with raving reviews of the goddess after some New\n\nAge witches conducted a ceremony involving me, Parvati, and Persephone, who he described as a\n\nPerhaps it was Parvati's love for Shiva that inspired me to seduce him, the great usurper\n\nNergal. Locusts swarmed from his skin, and tumors raged in his loins. He was the only god to have\n\nvisited me since Ishtar's failed attempted at imperialistic domination (another story for another\n\nday.) The people called him the God of pestilence and disease, of famine and cruelty. That was\n\nfine by my standards, after all, an internet search will bring forth my title of “the self­pitying\n\ngoddess of the underworld.” Which is also fine, for pity is tied to mercy, and believe me, I could\n\nbe much worse. Nergal first came down by edict of the divine council, he was to bring me tasty\n\ndelights from the realms I am unable to visit. Most of the stories say that I intended him to see me\n\nget out of the bath, naked, and they are correct, for as you will remember, my innocence stayed in\n\nthe skies. Though the delights turned to ash, our love making woke me up to a pleasure I did not\n\nbelieve could exist in these dismal halls. I loved him for this, or at least felt a yearning that tore at\n\nmy heart like hungry claws into fresh meat.\n\nI now understand this type of pleasure is well known amongst the anthropomorphs of the\n\nupper realms. It was of little deviation from his standard being, it was all the contact I had ever\n\nknown. To make a long story short, I demanded his presence in my realm, how could they deprive\n\nmore than they already had? Nergal was sent back to me, his tongue spewing malaria, his belly\n\nemaciated from his own famine. The story goes he entered my throne room, grasped me by my\n\nlong black tendrils, threw me to the floor, he took my form, then sat upon my throne. The story is\n\nmore or less accurate, though it leaves out a few details that would have delighted those horror\n\nloving medieval artists if that God of Abraham hadn't been such a greedy bastard. When he sat on\n\nmy throne the dead all bowed, a gesture they had never given to me. In his horror they saw the\n\nmakings of their own fates, they saw the very catalyst that had brought them to what had once been\n\nSlowly my cult dwindled, the archaeological record will tell you that I speak the truth.\n\nThe living came to worship him in my place, my throne became his. I am little more than a\n\nhandmaiden and fuck toy to the reigning Mesopotamian God of the dead. This is fine by me. For it\n\nbrings you to the moral of this story, and listen closely, because a moral from a Goddess is as\n\neternal as belief. When stolen, raped, and dethroned, always tell your story. Pity is power too. Just\n\nlight a candle for me tonight, we will see who rises to a great throne in the sky.\n"
+var text = "﻿Ereshkiga\r\n        To begin, I never asked to be the ruler of this realm. I was satisfied with my status as a sky deity from the moment my father's tears conceived me in the womb of mother ocean. Innocence lives in the sky, and innocent I was. In an eternal whim I could dissolve into clarity, come back as a cloud, watch and listen to the above and below. What a time that was.\r\n        They say all good things must come to an end, and so it did, just look what the Jews did to us with their “monotheism.” Our powers drained away into one megalomaniac of a deity that no one in the heavenly realm payed much attention to, until, with bitter luck he stumbled upon some influential couple in a tent, whispered in their ears, and sparked our demise, disintegration, and  the  destruction of our temples.\r\n        But demise, disintegration, and destruction are known to be among my most esteemed traits. Not as a sky deity of course, that has more to do with those human ideals that the vast majority of you put onto a pampered pedestal, while the honest minority are relegated to worship my current status at heavy metal shows and psy trance parties deep in the night.\r\n        The legend goes that it was the dragon Kun that did it to me, the first betrayal by one who was supposed to love me. He was my brother, though it is hard to track down proper familial relations when the mere mingling of tears and water can bring about birth. I was in a moment of reconfiguration,  transitioning from pure blue dissipation to a more watery form, when a net fine enough to catch the air itself pulled me. I felt myself sweeping downward, it was the fall of the very innocence and  kindness that had once formed me into a shady cumulous for a desert traveler. With each meter of decent I felt myself solidifying into a ruling form, and a ruler I was to be. By the time Kun deserted me on the tombstone throne, I was  fully anthropomorphic, and still am today.\r\n        Being the ruler of the underworld had its joys. Law giving came easily to me, as well as giving judgements, holding down security matters, and so on and so forth. Perhaps that is why no one has come to my rescue, they recognize my utmost capacity to preside over the dead. I do like to watch them dance though, swaying forms, slowing decomposing, a twist to the left as the arms flings off to the right. The dead love to dance here, it speeds up the process of decomposition, and no one wants to stay here too long. There are more ethereal realms to be explored, where body is but a hazy memory cloaked in poetry.\r\n        How many I have seen, kings and emperors, those worshiped as idols, all come here, until they are returned to pure earth, no doubt feeding the all loving powers of my bitch of a sister Ishtar, “oh great bringer of life and abundance!”\r\n         I should be the goddess of life and abundance, without my kingdom how would your gardens grow? I hear Shiva had a similar existential crisis once, that God of destruction, he become distraught confiding in his dear Parvati, “Oh love, what am I? Only destruction? where is my goodness?” “Lord” she is said to have answered, “Without your destruction where would creation be, you are destruction, but from you creation is birthed!” What a wise wise goddess, no one in my pantheon gives me such credit. I admire her beautiful combination of love and wisdom, though I never got the chance to meet her in person (I am not allowed to leave my subterranean realm). But my great servant Neti came  back with raving reviews of the goddess after some New Age witches conducted a ceremony involving me, Parvati, and Persephone, who he described as a more balanced version of myself.\r\n        Perhaps it was Parvati's love for Shiva that inspired me to seduce him, the great usurper Nergal. Locusts swarmed from his skin, and tumors raged in his loins. He was the only god to have visited me since Ishtar's failed attempted at imperialistic domination (another story for another day.) The people called him the God of pestilence and disease, of famine and cruelty. That was fine by my standards, after all, an internet search will bring forth my title of “the self-pitying goddess of the underworld.” Which is also fine, for pity is tied to mercy, and believe me, I could be much worse. Nergal first came down by edict of the divine council, he was to bring me tasty delights from the realms I am unable to visit. Most of the stories say that I intended him to see me get out of the bath, naked, and they are correct, for as you will remember, my innocence stayed in the skies. Though the delights turned to ash, our love making woke me up to a pleasure I did not believe could exist in these dismal halls. I loved him for this, or at least felt a yearning that tore at my heart like hungry claws into fresh meat.\r\n        I now understand this type of pleasure is well known amongst the anthropomorphs of the upper realms. It was of little deviation from his standard being, it was all the contact I had ever known. To make a long story short, I demanded his presence in my realm, how could they deprive more than they already had? Nergal was sent back to me, his tongue spewing malaria, his belly emaciated from his own famine. The story goes he entered my throne room, grasped me by my long black tendrils, threw me to the floor, he took my form, then sat upon my throne. The story is more or less accurate, though it leaves out a few details that would have delighted those horror loving medieval artists if that God of Abraham hadn't been such a greedy bastard. When he sat on my throne the dead all bowed, a gesture they had never given to me. In his horror they saw the makings of their own fates, they saw the very catalyst that had brought them to what had once been my kingdom.\r\n        Slowly my cult dwindled, the archaeological record will tell you that I speak the truth. The living came to worship him in my place, my throne became his. I am little more than a handmaiden and fuck toy to the reigning Mesopotamian God of the dead. This is fine by me. For it brings you to the moral of this story, and listen closely, because a moral from a Goddess is as eternal as belief. When stolen, raped, and dethroned, always tell your story. Pity is power too. Just light a candle for me tonight, we will see who rises to a great throne in the sky."
 
 console.log(ui)
 
@@ -23,13 +23,30 @@ styles.fontSize = '24px'
 styles.letterSpacing = '3px'
 styles.lineHeight = '24px'
 
+//for(x in ui) document.body.removeChild(ui[x])
+
+var max = parseInt(getCSS(ui.leftFront, 'width'))
+
+console.log(max)
+
 module.exports = function(text, styles){
+  graph(text.split(/[\n\r]/))
   var dict = alpha(styles)
   var alphaDict = alpha(text, styles)
   console.log(dict)
   console.log(alphaDict)
 }
 
+function graph(text){
+  var c = true
+  var i = 0
+  var str = '<p>'
+}
+
+
+function getCSS(el, prop){
+  return document.defaultView.getComputedStyle(el).getPropertyValue(prop)
+}
 
 },{"getids":3,"jtype":4}],3:[function(require,module,exports){
 module.exports = function(el){
@@ -74,18 +91,22 @@ var punc = ',./;"\'<>?:"{}|!@#$%^&*()-=_+ '
 module.exports = function(string, params){
   if(!('string' == typeof string)) {
     params = string || {}
-    var a = typify(alpha, params);
-    var p = typify(punc, params)
+    var aa = typify(alpha, params);
+    var pp = typify(punc, params)
     var dict = {};
-    dict.average = {alphabet: a.average, punctuation: p.average}
-    for(var x in a) dict[x] = a[x]
-    for(var y in p) dict[y] = p[y]
-    dict.alphabet = a;
-    dict.punctuation = p;
+    dict.average = {alphabet: aa.average, punctuation: pp.average}
+    for(var x in aa) dict[x] = aa[x]
+    for(var y in pp) dict[y] = pp[y]
+    dict.alphabet = aa;
+    dict.punctuation = pp;
+//    document.body.removeChild(p)
     return dict
   }
   else{
-    return typify(string, params)
+
+    var d =  typify(string, params)
+//    document.body.removeChild(p)
+    return d
   }
 }
 
@@ -116,7 +137,6 @@ function typify(string, params){
   dict.average = {height: sumHeight / alphabet.length, width: sumWidth / alphabet.length}
   dict.total = styleMap(string);
 
-  document.body.removeChild(p)
   return dict
 
   function styleMap(e,i){
