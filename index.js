40fbe6955f38d2a83f3eb25211e3c7124f171bd3
johnnyscript <mostmodernist@gmail.com>
2015-08-28 17:33:53 -0700

    loc change

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var marked = require('marked')
 var alpha = require('jtype')
 var ui = require('getids')()
 
@@ -9,22 +10,32 @@ styles.lineHeight = '24px'
 
 //for(x in ui) document.body.removeChild(ui[x])
 
+
 var max = parseInt(getCSS(ui.leftFront, 'width'))
 
-console.log(max)
-
-module.exports = function(text, styles){
+module.exports = function(text){
   graph(text.split(/[\n\r]/))
   var dict = alpha(styles)
   var alphaDict = alpha(text, styles)
-  console.log(dict)
+//  console.log(dict)
   console.log(alphaDict)
 }
 
+function writeLine(arr, str){
+  str += arr.shift()
+  var dict = alpha(str)
+  var l = dict.total[0]
+  if(l < max && arr.length) {
+    str += ' '
+    return writeLine(arr, str)
+  }
+  else return str
+}
+
 function graph(text){
-  var c = true
-  var i = 0
-  var str = '<p>'
+console.log(text)
+  var str = writeLine(text[2].split(/\s/).filter(Boolean), '')
+  console.log(str)
 }
 
 
